58751db907389200e8d291a3e6c0358c72926bdc
kpmisthah <kpmisthah6@gmail.com>
2026-02-01 17:26:11 +0530

    feat(backend): implement UserRepository for data access

---
+++ src/repositories/user.repository.js
@@ -0,0 +1,36 @@
+const prisma = require('../config/prisma');
+
+class UserRepository {
+    async create(userData, select = null) {
+        return prisma.user.create({
+            data: userData,
+            select
+        });
+    }
+
+    async findByEmail(email) {
+        return prisma.user.findUnique({
+            where: { email },
+        });
+    }
+
+    async findById(id) {
+        return prisma.user.findUnique({
+            where: { id },
+        });
+    }
+
+    async findAll(options = {}) {
+        return prisma.user.findMany(options);
+    }
+
+    async update(id, data, select = null) {
+        return prisma.user.update({
+            where: { id },
+            data,
+            select
+        });
+    }
+}
+
+module.exports = new UserRepository();
